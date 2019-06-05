6e94745cc3786882cd91e20853e9623fc288aadb
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-06-05 16:58:49 +0930

    work how to deal with data outside trial

--- Danila_reformat_week1.R
+++ Danila_reformat_week1.R
@@ -5,11 +5,16 @@ library(readxl)
 library(lubridate)
 library(ggplot2)
 library(readr)
+library(sp)
+library(biogeo)
+library(stringr)
+library(rgdal)
+library(sf)
+####################################################################################################################
+###################       week 1                                      ##############################################
 
 
-##### week 1 ######
-#now connected to GIT hub
-setwd("c:/Users/ouz001/pasture_utilisation/Take2")
+#setwd("c:/Users/ouz001/pasture_utilisation/Take2")
 
 
 #Bring in csv files with new format
@@ -22,24 +27,24 @@ setwd("c:/Users/ouz001/pasture_utilisation/Take2")
 ###################                   DAY1 EF sheep codes            ##############################################
 ####################################################################################################################
 
-codes_EF_Group_1_d1<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d1.csv",skip=33)
+codes_EF_Group_1_d1<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d1.csv",skip=34)
 codes_EF_Group_1_d1 <- codes_EF_Group_1_d1[1:12, 1:2]
 codes_EF_Group_1_d1 <- mutate(codes_EF_Group_1_d1, trksegID = as.integer(ID))
 glimpse(codes_EF_Group_1_d1)
 
-codes_EF_Group_1_d2 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d2.csv", skip=33)
+codes_EF_Group_1_d2 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d2.csv", skip=34)
 codes_EF_Group_1_d2 <- codes_EF_Group_1_d2[1:12, 1:2]
 codes_EF_Group_1_d2 <- mutate(codes_EF_Group_1_d2, trksegID1 = as.character(ID))
 codes_EF_Group_1_d2 <- mutate(codes_EF_Group_1_d2, trksegID = as.integer(trksegID1))
 
-codes_EF_Group_1_d3 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d3.csv", skip=33)
+codes_EF_Group_1_d3 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d3.csv", skip=34)
 codes_EF_Group_1_d3 <- codes_EF_Group_1_d3[1:12,1:2]
 codes_EF_Group_1_d3 <- mutate(codes_EF_Group_1_d3, trksegID = as.integer(ID))
 codes_EF_Group_1_d3 <- mutate(codes_EF_Group_1_d3, trksegID1 = as.character(ID))
 codes_EF_Group_1_d3 <- mutate(codes_EF_Group_1_d3, trksegID = as.integer(trksegID1))
 
 
-codes_EF_Group_1_d4 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d4.csv", skip=33)
+codes_EF_Group_1_d4 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d4.csv", skip=34)
 codes_EF_Group_1_d4 <- codes_EF_Group_1_d4[1:12,1:2]
 codes_EF_Group_1_d4 <- mutate(codes_EF_Group_1_d4, trksegID1 = as.character(ID))
 codes_EF_Group_1_d4 <- mutate(codes_EF_Group_1_d4, trksegID = as.integer(trksegID1))
@@ -48,22 +53,22 @@ codes_EF_Group_1_d4 <- mutate(codes_EF_Group_1_d4, trksegID = as.integer(trksegI
 ###################                   DAY1 VF sheep codes            ##############################################
 ####################################################################################################################
 
-codes_VF_Group_1_d1<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d1.csv", skip=33)
+codes_VF_Group_1_d1<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d1.csv", skip=34)
 codes_VF_Group_1_d1 <- codes_VF_Group_1_d1[1:12, 1:2]
 codes_VF_Group_1_d1 <- mutate(codes_VF_Group_1_d1, trksegID1 = as.character(ID))
 codes_VF_Group_1_d1 <- mutate(codes_VF_Group_1_d1, trksegID = as.integer(trksegID1))
 
-codes_VF_Group_1_d2 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d2.csv", skip=33)
+codes_VF_Group_1_d2 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d2.csv", skip=34)
 codes_VF_Group_1_d2 <- codes_VF_Group_1_d2[1:12, 1:2]
 codes_VF_Group_1_d2 <- mutate(codes_VF_Group_1_d2, trksegID1 = as.character(ID))
 codes_VF_Group_1_d2 <- mutate(codes_VF_Group_1_d2, trksegID = as.integer(trksegID1))
 
-codes_VF_Group_1_d3 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d3.csv", skip=33)
+codes_VF_Group_1_d3 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d3.csv", skip=34)
 codes_VF_Group_1_d3 <- codes_VF_Group_1_d3[1:12,1:2]
 codes_VF_Group_1_d3 <- mutate(codes_VF_Group_1_d3, trksegID1 = as.character(ID))
 codes_VF_Group_1_d3 <- mutate(codes_VF_Group_1_d3, trksegID = as.integer(trksegID1))
 
-codes_VF_Group_1_d4 <- read.csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d4.csv", skip=33)
+codes_VF_Group_1_d4 <- read.csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d4.csv", skip=34)
 codes_VF_Group_1_d4 <- codes_VF_Group_1_d4[1:12,1:2]
 codes_VF_Group_1_d4 <- mutate(codes_VF_Group_1_d4, trksegID1 = as.character(ID))
 codes_VF_Group_1_d4 <- mutate(codes_VF_Group_1_d4, trksegID = as.integer(trksegID1))
@@ -77,7 +82,7 @@ codes_VF_Group_1_d4 <- mutate(codes_VF_Group_1_d4, trksegID = as.integer(trksegI
 ####################################################################################################################
 
 ##################             Step 1 bring in the raw logged data   ##############################################
-EF_Group_1_d1<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d1.csv", skip=63) %>% 
+EF_Group_1_d1<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d1.csv", skip=64) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "EF_Group_1_d1.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -110,7 +115,6 @@ end_timeEF_Group_1_d1_filter_start <- hms('09:15:00') + hms('04:00:00')
 EF_Group_1_d1_filter_start_end <- filter(EF_Group_1_d1_filter_start, hms < (end_timeEF_Group_1_d1_filter_start))
 glimpse(EF_Group_1_d1_filter_start_end)
 
-#write.csv(EF_Group_1_d1_filter_start_end, "EF_Group_1_d1_filter_start_end.csv")
 glimpse(EF_Group_1_d1_filter_start)
 
 ##################        Step 4 filter data based on collar (not handheld)        ###################################
@@ -118,8 +122,33 @@ glimpse(EF_Group_1_d1_filter_start)
 EF_Group_1_d1_filter_start_end_collar <- filter(EF_Group_1_d1_filter_start_end, type == "collar")
 #write.csv(EF_Group_1_d1_filter_start_end_hh, "EF_Group_1_d1_filter_start_end_hh_R.csv")
 #write.csv(EF_Group_1_d1_filter_start_end_collar, "EF_Group_1_d1_filter_start_end_collar_R.csv") 
-glimpse(EF_Group_1_d1_filter_start_end_hh)
+glimpse(EF_Group_1_d1_filter_start_end_collar)
 
+
+####################          step 5 convert lat and longs to x and Y               ##################################
+
+
+
+#https://spatialreference.org/ref/epsg/gda94-mga-zone-56/
+#epsg projection 28356
+
+mapCRS <- CRS("+init=epsg:28356")     # 28356 = GDA_1994_MGA_Zone_56
+wgs84CRS <- CRS("+init=epsg:4326")   # 4326 WGS 84 - assumed for input lats and longs
+
+
+coordinates(EF_Group_1_d1_filter_start_end_collar) <- ~ lon + lat
+proj4string(EF_Group_1_d1_filter_start_end_collar) <- wgs84CRS   # assume input lat and longs are WGS84
+#make new object_1
+EF_Group_1_d1_filter_start_end_collar_1 <- spTransform(EF_Group_1_d1_filter_start_end_collar, mapCRS)
+#make new df_1
+EF_Group_1_d1_filter_start_end_collar = as.data.frame(EF_Group_1_d1_filter_start_end_collar_1) #this has the new coordinates projected !YES!!
+#make new df with point x and point y
+EF_Group_1_d1_filter_start_end_collar <- mutate(EF_Group_1_d1_filter_start_end_collar,
+                                                POINT_X = lon,  POINT_Y = lat )
+
+####################          step 6 export data                                   ##################################
+write_csv(EF_Group_1_d1_filter_start_end_collar, 
+          "W:/VF/pasture_utilisation/Take2/data_for_clipping/EF_Group_1_d1_filter_start_end_collar.csv") 
 
 ###################################################################################################################
 ###################                   DAY2 EF Group 1 - week 1          ###########################################  
@@ -127,7 +156,7 @@ glimpse(EF_Group_1_d1_filter_start_end_hh)
 
 ##################             Step 1 bring in the raw logged data   ##############################################
 
-EF_Group_1_d2<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d2.csv", skip=63) %>% 
+EF_Group_1_d2<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d2.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "EF_Group_1_d2.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -174,7 +203,7 @@ glimpse(EF_Group_1_d2_filter_start_end_hh)
 
 ##################             Step 1 bring in the raw logged data   ##############################################
 
-EF_Group_1_d3<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d3.csv", skip=63) %>% 
+EF_Group_1_d3<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d3.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "EF_Group_1_d3.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -217,7 +246,7 @@ glimpse(EF_Group_1_d3_filter_start_end_hh)
 
 ##################             Step 1 bring in the raw logged data   ##############################################
 
-EF_Group_1_d4<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d4.csv", skip=63) %>% 
+EF_Group_1_d4<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/EF_Group_1_d4.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "EF_Group_1_d4.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -270,7 +299,7 @@ glimpse(EF_Group_1_d4_filter_start_end_hh)
 
 
 
-VF_Group_1_d1<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d1.csv", skip=63) %>% 
+VF_Group_1_d1<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d1.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "VF_Group_1_d1.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -322,7 +351,7 @@ glimpse(VF_Group_1_d1_filter_start_end_hh)
 ##################             Step 1 bring in the raw logged data   ##############################################
 
 
-VF_Group_1_d2<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d2.csv", skip=63) %>% 
+VF_Group_1_d2<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d2.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "VF_Group_1_d2.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -368,7 +397,7 @@ glimpse(VF_Group_1_d2_filter_start_end_hh)
 ##################             Step 1 bring in the raw logged data   ##############################################
 
 
-VF_Group_1_d3<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d3.csv", skip=63) %>% 
+VF_Group_1_d3<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d3.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "VF_Group_1_d3.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -414,7 +443,7 @@ glimpse(VF_Group_1_d3_filter_start_end_hh)
 ##################             Step 1 bring in the raw logged data   ##############################################
 
 
-VF_Group_1_d4<- read_csv("C:/Users/ouz001/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d4.csv", skip=63) %>% 
+VF_Group_1_d4<- read_csv("W:/VF/pasture_utilisation/Take2/raw_data/Week 1/VF_Group_1_d4.csv", skip=63) %>% 
   select(ID, trksegID, lat, lon,  ele, time) %>% 
   mutate(file_name = "VF_Group_1_d4.csv") %>% 
   separate(file_name,into =  c("fence", "group", "week", "day_of_exp"),  sep = "_", remove = FALSE ) %>% 
@@ -460,10 +489,10 @@ glimpse(VF_Group_1_d4_filter_start_end_hh)
   
   
   
-  
-  
+#################################################################################################################################  
+############################               summary of files that have been clipped                ##############################  
 
-
+EF_Group_1_d1_clipped <- read_csv("W:/VF/pasture_utilisation/Take2/data_for_clipping/EF_Group_1_d1_clipped.txt")
 
 
 
